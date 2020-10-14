5875673a4a50e829721be2e67d629505f33940d8
Jeff.A.Cate <Jeff.A.Cate@gmail.com>
2020-10-14 06:33:51 -0500

    Entity Framework setup for project 1.

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,11 +12,11 @@ namespace Persistence
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.Entity<Movie>().HasData(
-                new Movie{ Title = "Harry Potter and the Sorcerer's Stone", ReleaseDate =  new System.DateTime(2001, 11, 16), Synopsis = "An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world." },
-                new Movie{ Title = "Harry Potter and the Chamber of Secrets", ReleaseDate =  new System.DateTime(2002, 11, 15), Synopsis = "An ancient prophecy seems to be coming true when a mysterious presence begins stalking the corridors of a school of magic and leaving its victims paralyzed." },
-                new Movie{ Title = "Harry Potter and the Prisoner of Azkaban", ReleaseDate =  new System.DateTime(2004, 6, 4), Synopsis = "Harry Potter, Ron and Hermione return to Hogwarts School of Witchcraft and Wizardry for their third year of study, where they delve into the mystery surrounding an escaped prisoner who poses a dangerous threat to the young wizard." }
-            );
+            // builder.Entity<Movie>().HasData(
+            //     new Movie{ Title = "Harry Potter and the Sorcerer's Stone", ReleaseDate =  new System.DateTime(2001, 11, 16), Synopsis = "An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world." },
+            //     new Movie{ Title = "Harry Potter and the Chamber of Secrets", ReleaseDate =  new System.DateTime(2002, 11, 15), Synopsis = "An ancient prophecy seems to be coming true when a mysterious presence begins stalking the corridors of a school of magic and leaving its victims paralyzed." },
+            //     new Movie{ Title = "Harry Potter and the Prisoner of Azkaban", ReleaseDate =  new System.DateTime(2004, 6, 4), Synopsis = "Harry Potter, Ron and Hermione return to Hogwarts School of Witchcraft and Wizardry for their third year of study, where they delve into the mystery surrounding an escaped prisoner who poses a dangerous threat to the young wizard." }
+            // );
         }
     }
 }
